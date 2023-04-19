49a02d1e63835310e3efa33681cd6084b2ba31c6
Habibou Moussa <habibou997@gmail.com>
2023-04-19 23:00:59 +0000

    added hi title and notification icon

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,6 +14,35 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                //Hi habibou !
+                Text(
+                  "Hi Habibou",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+
+                //Notification
+                Container(
+                  decoration: BoxDecoration(color: Colors.blue[600]),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
